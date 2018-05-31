fec4463234e72bb7b53bed719523db63ca9cf1d2
DamBai212 <damanybailey@icloud.com>
2018-05-31 12:49:51 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,13 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board
-  cell = "   "
-  separator = "|"
-  lines = "-----------"
+def display_board(board)
+separator = "|"
+lines = "-----------"
 
-  puts "#{cell}#{separator}#{cell}#{separator}#{cell}"
-  puts "#{lines}"
-  puts "#{cell}#{separator}#{cell}#{separator}#{cell}"
-  puts "#{lines}"
-  puts "#{cell}#{separator}#{cell}#{separator}#{cell}"
+puts " #{board[0]} #{separator} #{board[1]} #{separator} #{board[2]} "
+puts "#{lines}"
+puts " #{board[3]} #{separator} #{board[4]} #{separator} #{board[5]} "
+puts "#{lines}"
+puts " #{board[6]} #{separator} #{board[7]} #{separator} #{board[8]} "
 end
